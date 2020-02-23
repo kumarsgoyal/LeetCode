cf6fb674247bf277072602897d3c63f0a1a09f68
Sahil Kumar <43093347+kumarsgoyal@users.noreply.github.com>
2020-02-23 16:52:29 +0530

    Create Transpose Matrix.cpp

---
+++ Array/Transpose Matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& A) {
+        vector<vector<int>> res;
+        
+        int row = A.size();
+        int col = A[0].size();
+        
+        for(int i = 0; i < col; i++) {
+            vector<int> temp;
+            for(int j = 0; j < row; j++) {
+                temp.push_back(A[j][i]);
+            }
+            res.push_back(temp);
+        }
+        
+        return res;
+    }
+};
