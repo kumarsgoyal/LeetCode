cb6572655a85f15c4cf775816c2ea32c7584d8a8
Sahil Kumar <43093347+kumarsgoyal@users.noreply.github.com>
2020-03-19 20:54:55 +0530

    Create Increasing Order Search Tree 1.cpp

---
+++ Tree/Increasing Order Search Tree 1.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *curr;
+    
+    
+    void constructTree(TreeNode* root) {
+        if(root == NULL) {
+            return;
+        }
+        
+        constructTree(root->left);
+        
+        root->left = NULL;
+        curr->right = root;
+        curr = root;
+        constructTree(root->right);
+    }
+    
+    TreeNode* increasingBST(TreeNode* root) {
+        if(root == NULL) {
+            return NULL;
+        }
+        TreeNode *result = new TreeNode(0);
+        curr = result;
+        
+        constructTree(root);
+        
+        return result->right;
+    }
+};
