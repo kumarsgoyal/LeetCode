b44f47c526c6f43ac34134f9a2ef84069da094cf
Sahil Kumar <43093347+kumarsgoyal@users.noreply.github.com>
2020-03-20 17:46:22 +0530

    Create Remove All Adjacent Duplicates In String [3].cpp

---
+++ Stack/Remove All Adjacent Duplicates In String [3].cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeDuplicates(string S) {
+        string res;
+        
+        if(S.size() == 0) {
+            return "";
+        }
+        
+        for(int i = 0; i < S.length(); i++) {
+            if((res.empty() == 0) && (res.back() == S[i])) {
+                res.pop_back();
+            }
+            // if res is empty or last element res is not equal to current element 
+            // S[i] mean they are not adjacent till now
+            else {
+                res.push_back(S[i]);
+            }
+        }
+        
+        return res;
+    }
+};
