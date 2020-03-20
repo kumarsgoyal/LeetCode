4853d37376bfbcf27cebde60818088d0315eeba2
Sahil Kumar <43093347+kumarsgoyal@users.noreply.github.com>
2020-03-20 22:00:36 +0530

    Update Valid Parentheses [1].cpp

--- Stack/Valid Parentheses [1].cpp
+++ Stack/Valid Parentheses [1].cpp
@@ -4,7 +4,7 @@ public:
         stack<char> stk;
         
         for(int i = 0; i < s.length(); i++) {
-            if(stk.empty() || s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
                 stk.push(s[i]);
             }
             else {
